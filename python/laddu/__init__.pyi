0e3a4a6df73646db4775d58fe8216a104459b332
denehoffman <denehoffman@me.com>
2024-12-03 17:22:33 -0500

    feat: add basic implementation to read directly from ROOT files
    This method is much slower than reading a parquet file, and I don't know if there really is a way to speed that up without using a Rust-side crate. I'm going to put that on the back-burner, but this works for now if people are desperate.

--- python/laddu/__init__.pyi
+++ python/laddu/__init__.pyi
@@ -1,4 +1,5 @@
 from abc import ABCMeta, abstractmethod
+from pathlib import Path
 
 from laddu.amplitudes import Expression, Manager, constant, parameter
 from laddu.amplitudes.breit_wigner import BreitWigner
@@ -6,51 +7,63 @@ from laddu.amplitudes.common import ComplexScalar, PolarComplexScalar, Scalar
 from laddu.amplitudes.ylm import Ylm
 from laddu.amplitudes.zlm import Zlm
 from laddu.convert import convert_from_amptools
-from laddu.data import BinnedDataset, Dataset, open
+from laddu.data import BinnedDataset, Dataset, Event, open  # noqa: A004
 from laddu.likelihoods import NLL, LikelihoodManager, Status
 from laddu.utils.variables import Angles, CosTheta, Mandelstam, Mass, Phi, PolAngle, Polarization, PolMagnitude
 from laddu.utils.vectors import Vector3, Vector4
 
 from . import amplitudes, convert, data, utils
 
+__version__: str
+
 class Observer(metaclass=ABCMeta):
     @abstractmethod
     def callback(self, step: int, status: Status) -> tuple[Status, bool]: ...
 
-__version__: str
+def open_amptools(
+    path: str | Path,
+    tree: str = "kin",
+    *,
+    pol_in_beam: bool = False,
+    pol_angle: float | None = None,
+    pol_magnitude: float | None = None,
+    num_entries: int | None = None,
+) -> Dataset: ...
 
 __all__ = [
-    "__version__",
-    "convert",
-    "convert_from_amptools",
-    "Dataset",
-    "open",
-    "BinnedDataset",
-    "utils",
-    "data",
-    "amplitudes",
-    "Vector3",
-    "Vector4",
-    "CosTheta",
-    "Phi",
+    "NLL",
     "Angles",
-    "PolMagnitude",
-    "PolAngle",
-    "Polarization",
+    "BinnedDataset",
+    "BreitWigner",
+    "ComplexScalar",
+    "CosTheta",
+    "Dataset",
+    "Event",
+    "Expression",
+    "LikelihoodManager",
+    "Manager",
     "Mandelstam",
     "Mass",
-    "Manager",
-    "LikelihoodManager",
-    "NLL",
-    "Expression",
-    "Status",
     "Observer",
-    "parameter",
-    "constant",
-    "Scalar",
-    "ComplexScalar",
+    "Phi",
+    "PolAngle",
+    "PolMagnitude",
     "PolarComplexScalar",
+    "Polarization",
+    "Scalar",
+    "Status",
+    "Vector3",
+    "Vector4",
     "Ylm",
     "Zlm",
-    "BreitWigner",
+    "__version__",
+    "amplitudes",
+    "constant",
+    "convert",
+    "convert_from_amptools",
+    "data",
+    "open",
+    "open_amptools",
+    "parameter",
+    "utils",
 ]
