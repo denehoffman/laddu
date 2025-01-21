0609455ee19d438f2891315389e167b610e54e22
denehoffman <denehoffman@me.com>
2025-01-21 11:18:45 -0500

    ci: remove coverage for FFI code, since we test it with pytest anyway

--- crates/laddu-python/src/lib.rs
+++ crates/laddu-python/src/lib.rs
@@ -1,16 +1,22 @@
+#![cfg_attr(coverage, feature(coverage_attribute))]
 use pyo3::prelude::*;
 use pyo3::types::PyDict;
 
+#[cfg_attr(coverage, coverage(off))]
 pub mod amplitudes;
+#[cfg_attr(coverage, coverage(off))]
 pub mod data;
+#[cfg_attr(coverage, coverage(off))]
 pub mod utils;
 
+#[cfg_attr(coverage, coverage(off))]
 pub trait GetStrExtractObj {
     fn get_extract<T>(&self, key: &str) -> PyResult<Option<T>>
     where
         T: for<'py> FromPyObject<'py>;
 }
 
+#[cfg_attr(coverage, coverage(off))]
 impl GetStrExtractObj for Bound<'_, PyDict> {
     fn get_extract<T>(&self, key: &str) -> PyResult<Option<T>>
     where
