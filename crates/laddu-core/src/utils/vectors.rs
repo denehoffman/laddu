0531886193883b5aa5a22fc2153fb81b28854904
Nathaniel D. Hoffman <dene@cmu.edu>
2025-02-20 23:44:01 +0530

    test(vectors): add more vector test coverage

--- crates/laddu-core/src/utils/vectors.rs
+++ crates/laddu-core/src/utils/vectors.rs
@@ -460,8 +460,89 @@ impl std::iter::Sum<Vec4> for Vec4 {
 #[cfg(test)]
 mod tests {
     use approx::assert_relative_eq;
+    use nalgebra::{Vector3, Vector4};
 
     use super::*;
+
+    #[test]
+    fn test_display() {
+        let v3 = Vec3::new(1.2341, -2.3452, 3.4563);
+        assert_eq!(format!("{}", v3), "[ 1.234, -2.345,  3.456]");
+        let v4 = Vec4::new(1.2341, -2.3452, 3.4563, 4.5674);
+        assert_eq!(format!("{}", v4), "[ 1.234, -2.345,  3.456;  4.567]");
+    }
+
+    #[test]
+    fn test_vec_vector_conversion() {
+        let v = Vec3::new(1.0, 2.0, 3.0);
+        let vector3: Vec<Float> = v.into();
+        assert_eq!(vector3[0], 1.0);
+        assert_eq!(vector3[1], 2.0);
+        assert_eq!(vector3[2], 3.0);
+
+        let v_from_vec: Vec3 = vector3.into();
+        assert_eq!(v_from_vec, v);
+
+        let v = Vec4::new(1.0, 2.0, 3.0, 4.0);
+        let vector4: Vec<Float> = v.into();
+        assert_eq!(vector4[0], 1.0);
+        assert_eq!(vector4[1], 2.0);
+        assert_eq!(vector4[2], 3.0);
+        assert_eq!(vector4[3], 4.0);
+
+        let v_from_vec: Vec4 = vector4.into();
+        assert_eq!(v_from_vec, v);
+    }
+
+    #[test]
+    fn test_vec_array_conversion() {
+        let arr = [1.0, 2.0, 3.0];
+        let v: Vec3 = arr.into();
+        assert_eq!(v, Vec3::new(1.0, 2.0, 3.0));
+
+        let back_to_array: [Float; 3] = v.into();
+        assert_eq!(back_to_array, arr);
+
+        let arr = [1.0, 2.0, 3.0, 4.0];
+        let v: Vec4 = arr.into();
+        assert_eq!(v, Vec4::new(1.0, 2.0, 3.0, 4.0));
+
+        let back_to_array: [Float; 4] = v.into();
+        assert_eq!(back_to_array, arr);
+    }
+
+    #[test]
+    fn test_vec_nalgebra_conversion() {
+        let v = Vec3::new(1.0, 2.0, 3.0);
+        let vector3: Vector3<Float> = v.into();
+        assert_eq!(vector3.x, 1.0);
+        assert_eq!(vector3.y, 2.0);
+        assert_eq!(vector3.z, 3.0);
+
+        let v_from_vec: Vec3 = vector3.into();
+        assert_eq!(v_from_vec, v);
+
+        let v = Vec4::new(1.0, 2.0, 3.0, 4.0);
+        let vector4: Vector4<Float> = v.into();
+        assert_eq!(vector4.x, 1.0);
+        assert_eq!(vector4.y, 2.0);
+        assert_eq!(vector4.z, 3.0);
+        assert_eq!(vector4.w, 4.0);
+
+        let v_from_vec: Vec4 = vector4.into();
+        assert_eq!(v_from_vec, v);
+    }
+
+    #[test]
+    fn test_vec_sums() {
+        let vectors = [Vec3::new(1.0, 2.0, 3.0), Vec3::new(4.0, 5.0, 6.0)];
+        let sum: Vec3 = vectors.iter().sum();
+        assert_eq!(sum, Vec3::new(5.0, 7.0, 9.0));
+
+        let vectors = [Vec4::new(1.0, 2.0, 3.0, 4.0), Vec4::new(4.0, 5.0, 6.0, 7.0)];
+        let sum: Vec4 = vectors.iter().sum();
+        assert_eq!(sum, Vec4::new(5.0, 7.0, 9.0, 11.0));
+    }
 
     #[test]
     fn test_three_to_four_momentum_conversion() {
