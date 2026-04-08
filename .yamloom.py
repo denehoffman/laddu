03e4832baa003a2cd657b49a981d9c475a064017
Nathaniel D. Hoffman <36977879+denehoffman@users.noreply.github.com>
2026-04-08 16:24:51 -0500

    ci: fix test build workflow

--- .yamloom.py
+++ .yamloom.py
@@ -293,7 +293,6 @@ test_build_workflow = Workflow(
             ],
             name='Build Source Distribution',
             runs_on='ubuntu-22.04',
-            needs=['build-check-test'],
             condition=context.github.ref.startswith('refs/tags/')
             | (context.github.event_name == 'workflow_dispatch'),
         ),
